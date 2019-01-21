c5153ae2ec3f23c13c86d0d01bbe6b1107986ad2
Tyrone <33449063+Jooelk@users.noreply.github.com>
2019-01-21 16:05:48 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+loadscreen 'index.html'
+description ''
+version '1.0.0'
+
+files {
+    'index.html',
+    'style.css',
+    'app.js',
+    'img/bg1',
+    'img/logo.png',
+    'music/logo.png',
+
+}
